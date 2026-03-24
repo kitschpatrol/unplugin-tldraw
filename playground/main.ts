2f549ee5d8f9c7cf45d1303026ff7efba6a0958f
Eric Mika <eric@ericmika.com>
2026-03-24 17:34:36 -0400

    Dark test.

--- playground/main.ts
+++ playground/main.ts
@@ -1,5 +1,11 @@
+/* eslint-disable import/no-duplicates */
 import imageStatic from '../test/assets/test-sketch.tldr'
+import imageStaticDark from '../test/assets/test-sketch.tldr?dark=true&tldr'
 
 const imageStaticElement = document.createElement('img')
 imageStaticElement.src = imageStatic
 document.body.append(imageStaticElement)
+
+const imageStaticDarkElement = document.createElement('img')
+imageStaticDarkElement.src = imageStaticDark
+document.body.append(imageStaticDarkElement)
